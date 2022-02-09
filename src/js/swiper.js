ebc6d8a0bbabbfa8ce436f92787d73112306af43
reutki <marcu.reutki@gmail.com>
2022-02-09 13:10:13 +0200

    [journey]: added slider component

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,19 +1,14 @@
 import Swiper, { Navigation, Pagination } from "swiper";
 
 let Menu = ["1996-2004", "2005-2009", "2010-2014", "2015-2021", "2021-2030"];
-const swiper = new Swiper(".swiper", {
+var swiper = new Swiper(".swiper", {
   speed: 400,
-  direction: "horizontal",
+  direction: "vertical",
   //   loop: true,
-  pagination: {
-    el: ".years-axis",
-    clickable: true,
-  },
   initialSlide: 0,
   centeredSlides: true,
   effect: "fade",
+  init: "true",
 });
 const mySwiper = document.querySelector(".swiper").swiper;
 let index = document.querySelector(".data-swiper-slide-index");
-
-//TODO: create pagination function for the slider.
