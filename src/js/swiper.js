7a2715a2f8b406cc6af875637fabfaa3c46954b1
reutki <marcu.reutki@gmail.com>
2022-02-10 14:03:00 +0200

    [journey]: created pagination

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,10 +1,23 @@
-import Swiper, { Navigation, Pagination } from "swiper";
-let index = [0];
+import Swiper, { Navigation, Pagination, EffectFade } from "swiper";
+// Swiper.use([Pagination, Navigation]); varianta veche
 
-var swiper = new Swiper(".swiper-container", {
-  direction: "vertical",
-  initialSlide: 0,
+let Years = ["1996-2004", "2005-2009", "2010-2014", "2015-2021", "2021-2030"];
+var swiper = new Swiper(".swiper", {
+  modules: [Pagination, Navigation, EffectFade],
+  effect: "fade",
+  speed: 500,
+  fadeEffect: {
+    crossFade: true,
+  },
+
   pagination: {
     el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+    bulletClass: "Bullet",
+    bulletActiveClass: "BulletActive",
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + Years[index] + "</span>";
+    },
   },
 });
