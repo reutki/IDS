b377a044020b3fc2eafe66ff7d0108e06b4f3c8d
reutki <marcu.reutki@gmail.com>
2022-02-10 10:58:42 +0200

    [journey]:changed html and css of slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,14 +1,10 @@
 import Swiper, { Navigation, Pagination } from "swiper";
+let index = [0];
 
-let Menu = ["1996-2004", "2005-2009", "2010-2014", "2015-2021", "2021-2030"];
-var swiper = new Swiper(".swiper", {
-  speed: 400,
+var swiper = new Swiper(".swiper-container", {
   direction: "vertical",
-  //   loop: true,
   initialSlide: 0,
-  centeredSlides: true,
-  effect: "fade",
-  init: "true",
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
-const mySwiper = document.querySelector(".swiper").swiper;
-let index = document.querySelector(".data-swiper-slide-index");
