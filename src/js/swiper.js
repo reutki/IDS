7e4a258bd0d0fa6873f13d33b5b8860307310c53
reutki <marcu.reutki@gmail.com>
2022-02-11 02:13:42 +0200

    [swiper]: reinitialized the layout of the component

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,23 +1,23 @@
-import Swiper, { Navigation, Pagination, EffectFade } from "swiper";
-// Swiper.use([Pagination, Navigation]); varianta veche
-
+import Swiper, { Navigation, Pagination } from "swiper";
+import $ from "jquery";
 let Years = ["1996-2004", "2005-2009", "2010-2014", "2015-2021", "2021-2030"];
+
 var swiper = new Swiper(".swiper", {
-  modules: [Pagination, Navigation, EffectFade],
-  effect: "fade",
-  speed: 500,
-  fadeEffect: {
-    crossFade: true,
-  },
+  modules: [Pagination, Navigation],
+  direction: "horizontal",
+  slidesPerView: 1,
+  addSlidesAfter: 1,
 
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
     clickable: true,
-    bulletClass: "Bullet",
-    bulletActiveClass: "BulletActive",
+    hideOnClick: true,
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + Years[index] + "</span>";
+      return (
+        '<span class="' + className + '">' + Years[index] + "</span>"
+        // console.log("Info:" + className + " " + Years[index])
+      );
     },
   },
 });
