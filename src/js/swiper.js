f75574a08a801d8f2f3026da1855356f3d5f9f58
reutki <marcu.reutki@gmail.com>
2022-02-11 16:44:58 +0200

    [journey]: added desktop,tablet for journey-content

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,21 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-// import $ from 'jquery';
-let Years = ['1996-2004', '2005-2009', '2010-2014', '2015-2021', '2021-2030'];
+import Swiper, { Navigation, Pagination, EffectFade } from "swiper";
+let Years = ["1996-2004", "2005-2009", "2010-2014", "2015-2021", "2021-2030"];
 
-var swiper = new Swiper('.journey-slider', {
-  modules: [Pagination, Navigation],
-  // direction: 'horizontal',
-  // slidesPerView: 1,
-  // addSlidesAfter: 1,
+var swiper = new Swiper(".journey-slider", {
+  modules: [Pagination, Navigation, EffectFade],
+  effect: "fade",
+  speed: 1000,
+  fadeEffect: {
+    crossFade: true,
+  },
 
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     // type: 'bullets',
     clickable: true,
     // hideOnClick: true,
-    bulletClass: 'journey-bullet',
-    bulletActiveClass: 'is-active',
+    bulletClass: "journey-bullet",
+    bulletActiveClass: "is-active",
     renderBullet: function (index, className) {
       return `<button type="button" class="${className}">${Years[index]}</button>`;
     },
